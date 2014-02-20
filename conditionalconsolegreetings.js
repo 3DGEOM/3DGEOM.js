afb1b4968623b61f9818437c12386b87bc5e3cfc
3DGEOM <3dgeom@gmail.com>
2014-02-20 06:18:31 -0500

    Create conditionalconsolegreetings.js
    This is a simple if else statement that prints greetings to the console depending on the input into the conditional statement

---
+++ conditionalconsolegreetings.js
@@ -0,0 +1,20 @@
+// The following syntax can be modified to print to the console.log depending on the time of day in military hours.
+// The time should be a value between 0000 and 2400
+
+var CurrentTime = ("0800")
+
+//if else statement 
+
+if (time<1200)
+  {
+  x="Good morning"
+  }
+  
+else
+  {
+  x="Good evening"
+  }
+  
+//the appropriate greeting can be printed to the console.
+  
+  console.log(x)
